915e94eb05bbade43d65edfb1c36a0048f5921eb
Illia Katerynych <iliakat.kr@gmail.com>
2026-01-21 22:06:53 +0000

    feature: command line arguments

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,29 @@ use ignores::Ignore;
 struct Args {
     /// Folder to digest
     #[arg(short, long, default_value = ".")]
-    folder: String,
+    directory: String,
+
+    /// Path to ignore file
+    #[arg(short, long, default_value = "./.gitignore")]
+    ignore: String,
+
+    /// Output file tree structure
+    #[arg(short, long, default_value_t = false, action = clap::ArgAction::SetTrue)]
+    tree: bool,
+
+    /// Output file contents
+    #[arg(short, long, default_value_t = false, action = clap::ArgAction::SetTrue)]
+    files: bool,
 }
 
 fn main() {
     let args = Args::parse();
-    let ignore = Ignore::new(format!("{}/.gitignore", args.folder).as_str());
-    let file_tree = PathTree::new(&args.folder, &ignore);
-    file_tree.print_tree();
-    file_tree.print_files();
+    let ignore = Ignore::new(&args.ignore);
+    let file_tree = PathTree::new(&args.directory, &ignore);
+    if args.tree {
+        file_tree.print_tree();
+    }
+    if args.files {
+        file_tree.print_files();
+    }
 }
